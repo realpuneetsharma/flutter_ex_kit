2fd9dd8042bde4b6baf403d9f53b5ccd2ec13b8b
puneetflutter <puneet.dev@colourmoon.com>
2025-01-09 11:40:50 +0530

    add padding

--- lib/flutter_ex_kit.dart
+++ lib/flutter_ex_kit.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_ex_kit/single_ton.dart';
 
-part 'functions/output.dart';
 
 part 'ui/string_text_extensions.dart';
 part 'ui/scaffold_extensions.dart';
@@ -15,15 +14,18 @@ part 'ui/num_extensions.dart';
 part 'ui/overscroll_off_extension.dart';
 part 'ui/alignment_widget_extension.dart';
 part 'ui/network_image.dart';
+part 'ui/padding.dart';
 
 part 'functions/scrollable_extension.dart';
 part 'functions/exit_app.dart';
 part 'functions/pagination_function.dart';
 part 'functions/page_controller_extensions.dart';
+part 'functions/output.dart';
 
 part 'responsive/media_query_values.dart';
-part 'enums/file_type_enum.dart';
 part 'theme/color.dart';
 part 'animation/fade_in_animation.dart';
 part 'animation/bottom_animation.dart';
+
 part 'enums/fade_in_animation_enum.dart';
+part 'enums/file_type_enum.dart';
---
+++ lib/ui/padding.dart
@@ -0,0 +1,27 @@
+
+part of '../flutter_ex_kit.dart';
+extension PaddingExtension on Widget {
+  // Creates Padding with all sides padded equally
+  Widget all(double value) {
+    return Padding(
+      padding: EdgeInsets.all(value),
+      child: this,
+    );
+  }
+
+  // Creates Padding with symmetric vertical and horizontal padding
+  Widget symmetric({double vertical = 0.0, double horizontal = 0.0}) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: vertical, horizontal: horizontal),
+      child: this,
+    );
+  }
+
+  // Creates Padding with specific padding for each side
+  Widget only({double left = 0.0, double top = 0.0, double right = 0.0, double bottom = 0.0}) {
+    return Padding(
+      padding: EdgeInsets.only(left: left, top: top, right: right, bottom: bottom),
+      child: this,
+    );
+  }
+}
